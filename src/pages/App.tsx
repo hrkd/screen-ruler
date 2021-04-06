8d033f5ad25260b2ff5cbfc74a4a06265dbb4896
hrkd <hiroki@hrkd.net>
2021-04-06 13:38:16 +0900

    update

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,45 +1,30 @@
 import React, { useEffect, useState, useCallback } from 'react';
 import logo from '../logo.svg';
 import '../styles/App.scss';
-import { useApi } from '../hooks/useApi';
-import { useTransitionEnd } from '../hooks/useTransitionEnd';
 import { delay } from '../utils/delay';
 import cn from 'classnames';
 
 function App() {
-  const [works] = useApi('works');
-  const [init, setInit] = useState<boolean>(false);
-  const [ready, transitionEnd, setTransitionTarget] = useTransitionEnd();
+  const [width, setWidth] = useState<number>(0);
+  const [height, setHeight] = useState<number>(0);
 
   useEffect(() => {
-    if (!ready) return;
-    (async () => {
-      console.log('start');
-      await delay(1000);
-      console.log('1000ms later');
-      setInit(true);
-      await transitionEnd;
-      alert('transition end');
-    })();
-  }, [ready]);
+    window.addEventListener('resize', resize);
+    resize();
+    return () => {
+      window.removeEventListener('resize', resize);
+    };
+  }, []);
+
+  const resize = () => {
+    setWidth(window.innerWidth);
+    setHeight(window.innerHeight);
+  };
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className={cn({ init: init })} ref={setTransitionTarget}>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        {works && works.map((work, index) => <p key={index}>{work.slug}</p>)}
-      </header>
+      <section className="w">innerWidth: {width}px</section>
+      <section className="h">innerHeight: {height}px</section>
     </div>
   );
 }
